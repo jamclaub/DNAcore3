2bb3a1cb6cd28a40e36d952fb0605279ca06f383
jamclaub <jamclaub@ut.utm.edu>
2019-10-21 16:10:18 -0500

    Another attempt to make it work

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DNA.NETCORE3._0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PreviewWindow Preview;
+        TrimmerWindow Trimmer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Preview = new PreviewWindow(this);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Preview.Show();
+            this.Hide();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Trimmer = new TrimmerWindow(this, 0, 0, 0);
+            Trimmer.Show();
+            this.Hide();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //win3.Show();
+            this.Hide();
+        }
+        private void Exit_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult chose = MessageBox.Show("Close Data Processor?", "Data Processor exit conformation.", MessageBoxButton.OKCancel);
+            switch (chose)
+            {
+                case MessageBoxResult.OK:
+                    Application.Current.Shutdown();
+                    break;
+                case MessageBoxResult.Cancel:
+                    break;
+            }
+        }
+    }
+}
---
+++ PreviewWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Defaults;
+
+namespace DNA.NETCORE3._0
+{
+    /// <summary>
+    /// Interaction logic for PreviewWindow.xaml
+    /// </summary>
+    public partial class PreviewWindow : Window
+    {
+        public ChartValues<ObservablePoint> ValuesA { get; set; }
+        public ChartValues<ObservablePoint> ValuesB { get; set; }
+        public ChartValues<ObservablePoint> ValuesC { get; set; }
+        public ChartValues<ObservablePoint> ValuesD { get; set; }
+        private MainWindow mainWindow;
+        public PreviewWindow(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+            ValuesA = new ChartValues<ObservablePoint>();
+            ValuesB = new ChartValues<ObservablePoint>();
+            ValuesC = new ChartValues<ObservablePoint>();
+            ValuesD = new ChartValues<ObservablePoint>();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Show();
+            this.Hide();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Previewer pre = new Previewer(ValuesA, ValuesB, ValuesC, ValuesD);
+            pre.runRandomSampler();
+            DataContext = this;
+        }
+
+        public double percentageData { get; set; }
+        private void ChartOnDataClick(object sender, ChartPoint point)
+        {
+            percentageData = point.Y;
+            string showPercentage = percentageData.ToString();
+            PercentageChoice.Text = showPercentage;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult choice = MessageBox.Show("Would you like to send your settings to the trimmer?", "Timmer conformation", MessageBoxButton.YesNo);
+            switch (choice)
+            {
+                case MessageBoxResult.Yes:
+                    TrimmerWindow Trimmer = new TrimmerWindow(mainWindow, percentageData, 0, 0);
+                    Trimmer.Show();
+                    this.Hide();
+                    break;
+                case MessageBoxResult.No:
+                    break;
+            }
+
+        }
+    }
+}
