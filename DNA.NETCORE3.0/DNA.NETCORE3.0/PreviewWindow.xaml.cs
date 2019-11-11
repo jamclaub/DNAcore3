1f485cf378c58a4a2d88c47af25cecd96aba519a
James Laubach <jamclaub@ut.utm.edu>
2019-11-10 21:51:33 -0600

    Offset types, some error checking

--- DNA.NETCORE3.0/DNA.NETCORE3.0/PreviewWindow.xaml.cs
+++ DNA.NETCORE3.0/DNA.NETCORE3.0/PreviewWindow.xaml.cs
@@ -27,32 +27,30 @@ namespace DNA.NETCORE3._0
         public double percentageData = 0;
         public double WindowSize = 0;
         public double WindowQualityChoice = 0;
-        
+        private bool SingleFileSelected = false;
+        private bool TwoFilesSelected = false;
+        private bool SangerBool = false;
+        private bool SolexaBool = false;
+        private bool IlluminaV1Bool = false;
+        private bool IlluminaV2Bool = false;
+        private bool CustomBool = false;
+        private string trimmername;
+        private int offset = 0;
         
         public PreviewWindow()
         {
             InitializeComponent();
-            MessageBoxResult result = MessageBox.Show("some instructions here");
+            //MessageBoxResult result = MessageBox.Show("some instructions here");
             ValuesA = new ChartValues<ObservablePoint>();
             ValuesB = new ChartValues<ObservablePoint>();
             ValuesC = new ChartValues<ObservablePoint>();
             ValuesD = new ChartValues<ObservablePoint>();
-
-            Percentage.Text = Convert.ToString(percentageData);
-            WSize.Text = Convert.ToString(WindowSize);
-            WQuality.Text = Convert.ToString(WindowQualityChoice);
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            
-            this.Hide();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Previewer pre1 = new Previewer(ValuesA, ValuesB, ValuesC, ValuesD);
-            Previewer pre2 = new Previewer(ValuesA, ValuesB, ValuesC, ValuesD);
+            
             if(SingleFileSelected && !TwoFilesSelected)
             {
                 pre1.runRandomSampler();
@@ -76,24 +74,28 @@ namespace DNA.NETCORE3._0
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-            MessageBoxResult choice = MessageBox.Show("Would you like to send your settings to the trimmer?", "Timmer conformation", MessageBoxButton.YesNo);
+            percentageData = Convert.ToDouble(Percentage.Text);
+            WindowSize = Convert.ToDouble(WSize.Text);
+            WindowQualityChoice = Convert.ToDouble(WQuality.Text);
+            
+            MessageBoxResult choice = MessageBox.Show("Quality of Nucleotides: " + percentageData + "\n" +
+                                                      "Window Size: " + WindowSize + "\n" +
+                                                      "Window Quality: " + WindowQualityChoice + "\n" +
+                                                      "Trimmer Version and offset: " + trimmername + " " + offset + "\n" +
+                                                      "Send these settings to the trimmer?","Settings", MessageBoxButton.YesNo);
+
             switch (choice)
             {
                 case MessageBoxResult.Yes:
-                    percentageData = Convert.ToDouble(Percentage.Text);
-                    WindowSize = Convert.ToDouble(WSize.Text);
-                    WindowQualityChoice = Convert.ToDouble(WQuality.Text);
-                    TrimmerWindow Trimmer = new TrimmerWindow(this, percentageData, WindowSize, WindowQualityChoice);
-                    Trimmer.Show();
-                    this.Hide();
+                    
+                    
                     break;
                 case MessageBoxResult.No:
                     break;
             }
         }
 
-        private bool SingleFileSelected = false;
-        private bool TwoFilesSelected = false;
+        
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             SingleFileSelected = true;
@@ -104,6 +106,98 @@ namespace DNA.NETCORE3._0
         {
             SingleFileSelected = false;
             TwoFilesSelected = true;
+        }
+
+        
+        private void Sanger_Checked(object sender, RoutedEventArgs e)
+        {
+            trimmername = "Sanger";
+            CustomOffset.IsReadOnly = true;
+            CustomOffset.Background = Brushes.SlateGray;
+            CustomOffset.BorderBrush = Brushes.SlateGray;
+            SangerBool = true;
+            SolexaBool = false;
+            IlluminaV1Bool = false;
+            IlluminaV2Bool = false;
+            offset = 33;
+        }
+
+        private void SolexaIllumina_Checked(object sender, RoutedEventArgs e)
+        {
+            trimmername = "Solexa/Illumina";
+            CustomOffset.IsReadOnly = true;
+            CustomOffset.Background = Brushes.SlateGray;
+            CustomOffset.BorderBrush = Brushes.SlateGray;
+            SangerBool = false;
+            SolexaBool = true;
+            IlluminaV1Bool = false;
+            IlluminaV2Bool = false;
+            offset = 59;
+        }
+
+        private void IlluminaV1_Checked(object sender, RoutedEventArgs e)
+        {
+            trimmername = "Illumina 1.3-1.5";
+            CustomOffset.IsReadOnly = true;
+            CustomOffset.Background = Brushes.SlateGray;
+            CustomOffset.BorderBrush = Brushes.SlateGray;
+            SangerBool = false;
+            SolexaBool = false;
+            IlluminaV1Bool = true;
+            IlluminaV2Bool = false;
+            offset = 64;
+        }
+
+        private void IlluminaV2_Checked(object sender, RoutedEventArgs e)
+        {
+            trimmername = "illumina 1.5-1.8";
+            CustomOffset.IsReadOnly = true;
+            CustomOffset.Background = Brushes.SlateGray;
+            CustomOffset.BorderBrush = Brushes.SlateGray;
+            SangerBool = false;
+            SolexaBool = false;
+            IlluminaV1Bool = false;
+            IlluminaV2Bool = true;
+            offset = 0;
+        }
+
+        private void Custom_Checked(object sender, RoutedEventArgs e)
+        {
+            trimmername = " Custom";
+            CustomOffset.IsReadOnly = false;
+            CustomOffset.Background = Brushes.White;
+            CustomOffset.BorderBrush = Brushes.White;
+            SangerBool = false;
+            SolexaBool = false;
+            IlluminaV1Bool = false;
+            IlluminaV2Bool = false;
+            CustomBool = true;
+        }
+
+        private void Percentage_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !InRange(((TextBox)sender).Text + e.Text);
+        }
+
+        private void WSize_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !InRange(((TextBox)sender).Text + e.Text);
+        }
+
+        private void WQuality_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !InRange(((TextBox)sender).Text + e.Text);
+        }
+
+        private void CustomOffset_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !InRange(((TextBox)sender).Text + e.Text);
+        }
+        
+        public bool InRange(string str)
+        {
+            double i;
+            return double.TryParse(str, out i) && i > -1 && i < 101;
         }
     }
 }
